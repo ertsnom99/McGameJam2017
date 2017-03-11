e52976f2063297ed917266eb4b2dbc8c90d2cf74
Duong Tony <tonyduong@live.fr>
2017-03-11 11:54:34 -0500

    Code adjusted

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    private bool isInfectedObject = true;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public bool IsInfectedObject()
+    {
+        return isInfectedObject;
+    }
+
+    public void SetInfectedObject(bool b)
+    {
+        isInfectedObject = b;
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,21 @@
-﻿using System.Collections;
-using UnityEngine;
 
-[RequireComponent(typeof(CharacterMovement))]
 
-public class PlayerController : MonoBehaviour {
-
-    public const string VERTICAL_INPUT = "VInput";
-    public const string HORIZONTAL_INPUT = "HInput";
-    public const string ACTION_INPUT = "AInput";
-    public const string INFECT_INPUT = "IInput";
-
-    private CharacterMovement movementScript;
-
+    private PlayerMovement movementScript;
+    private CharacterInteraction interactionScript;
+  
     private void Awake()
     {
         InitializeVariables();
-    }
-
-    private void InitializeVariables()
-    {
-        movementScript = GetComponent<CharacterMovement>();
-    }
-	
-	void Update () {
-        Hashtable inputs = fetchInputs();
         movementScript.moveCharacter(inputs);
-	}
+        if ((bool)inputs[ACTION_INPUT]) // interact with object
+        {
+            interactionScript.interact();
+        }
+        if ((bool)inputs[INFECT_INPUT])
+        {
+            interactionScript.infect();
+        }
+    }
 
     private Hashtable fetchInputs()
     {
@@ -34,8 +24,35 @@ public class PlayerController : MonoBehaviour {
         ht.Add(VERTICAL_INPUT, Input.GetAxis("Vertical"));
         ht.Add(HORIZONTAL_INPUT, Input.GetAxis("Horizontal"));
         ht.Add(ACTION_INPUT, Input.GetButtonDown("Fire1"));
-        ht.Add(INFECT_INPUT, Input.GetButtonDown("Fire3"));      
-
+        ht.Add(INFECT_INPUT, Input.GetButtonDown("Fire3"));    
+        
         return ht;
     }
-}
+    private PlayerMovement movementScript;
+    private CharacterInteraction interactionScript;
+  
+    private void Awake()
+    {
+        InitializeVariables();
+        movementScript.moveCharacter(inputs);
+        if ((bool)inputs[ACTION_INPUT]) // interact with object
+        {
+            interactionScript.interact();
+        }
+        if ((bool)inputs[INFECT_INPUT])
+        {
+            interactionScript.infect();
+        }
+    }
+
+    private Hashtable fetchInputs()
+    {
+        Hashtable ht = new Hashtable();
+
+        ht.Add(VERTICAL_INPUT, Input.GetAxis("Vertical"));
+        ht.Add(HORIZONTAL_INPUT, Input.GetAxis("Horizontal"));
+        ht.Add(ACTION_INPUT, Input.GetButtonDown("Fire1"));
+        ht.Add(INFECT_INPUT, Input.GetButtonDown("Fire3"));    
+        
+        return ht;
+    }
