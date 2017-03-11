90b39f13a6eef9f6b7d6deeef60b2bf96efa9228
Duong Tony <tonyduong@live.fr>
2017-03-11 14:03:50 -0500

    - Arts added to the scene && Walls && Areas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public const int CHARACTER_SPEED = 1;
+    public GameObject[] infectiveObjects;
+    public int numberBotCharacters = 20;
+    public TextMesh timerText;
+
+    private float timer = 180.0f;
+    
+
+    private void Update()
+    {
+        ManageTimer(); 
+    }
+
+    private void ManageTimer()
+    {
+        timer -= Time.deltaTime;
+        int minutes = (int) Mathf.Floor(timer / 60);
+        int seconds = (int) Mathf.Floor(timer % 60);
+        timerText.text = minutes + ":" + seconds;
+    }
 }
