76236dce83d0b198d117a09a364b95f041c6560d
Duong Tony <tonyduong@live.fr>
2017-03-10 23:36:14 -0500

    Character movement & player controls added

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private float speed;
+
+    private void Awake()
+    {
+        InitializeVariables();
+    }
+
+    private void InitializeVariables()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        speed = 5;
+    }
+
+    public void moveCharacter(Hashtable inputs)
+    {
+        Vector2 movement = new Vector2((float)inputs[PlayerController.HORIZONTAL_INPUT], (float)inputs[PlayerController.VERTICAL_INPUT]);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rb2d.position += movement;
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMovement))]
+public class PlayerController : MonoBehaviour {
+
+    public const string VERTICAL_INPUT = "VInput";
+    public const string HORIZONTAL_INPUT = "HInput";
+    public const string ACTION_INPUT = "AInput";
+    public const string INFECT_INPUT = "IInput";
+    private CharacterMovement movementScript;
+
+    private void Awake()
+    {
+        InitializeVariables();
+    }
+
+    private void InitializeVariables()
+    {
+        movementScript = GetComponent<CharacterMovement>();
+    }
+	
+	void Update () {
+        Hashtable inputs = fetchInputs();
+        movementScript.moveCharacter(inputs);
+	}
+
+    private Hashtable fetchInputs()
+    {
+        Hashtable ht = new Hashtable();
+
+        ht.Add(VERTICAL_INPUT, Input.GetAxis("Vertical"));
+        ht.Add(HORIZONTAL_INPUT, Input.GetAxis("Horizontal"));
+        ht.Add(ACTION_INPUT, Input.GetButtonDown("Fire1"));
+        ht.Add(INFECT_INPUT, Input.GetButtonDown("Fire3"));      
+
+        return ht;
+    }
+}
